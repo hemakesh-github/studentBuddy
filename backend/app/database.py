2b0cf7b92f826925bd9eea1295f0a8d2aea0755b
Hemakesh reddy Muppdi <hemakesh8333@gmail.com>
2025-06-23 13:52:54 +0530

    sec fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Update the database URL with the provided connection details
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_ytgB2UKeE-RQJhp_EMu@mysql-2bf14153-hemakesh8333-ce33.b.aivencloud.com:24682/defaultdb"
+SQLALCHEMY_DATABASE_URL =  os.getenv("DATABASE_URI")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"connect_timeout": 10})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
